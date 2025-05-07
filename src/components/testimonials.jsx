9f77cdd881d3fffdbf6f29c22c5ecfcb86482015
Tomás <97319177+tomjzs@users.noreply.github.com>
2025-05-07 00:35:59 -0300

    testimonios upload

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -5,7 +5,7 @@ export const Testimonials = (props) => {
     <div id="testimonials">
       <div className="container">
         <div className="section-title text-center">
-          <h2>Nuestros clientes</h2>
+          <h2>Testimonios</h2>
         </div>
         <div className="row">
           {props.data
